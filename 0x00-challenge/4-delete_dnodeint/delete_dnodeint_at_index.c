01f37317cd90bcc10c72e8d0449996133a215587
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-07-21 23:34:20 +0100

    code fix

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,43 +1,53 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Deletes a node at a given index in a doubly linked list.
- * @head: Pointer to the head node of the list.
- * @index: Index of the node to be deleted.
- * Return: 1 if success, -1 if failure.
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *temp, *current = *head;
+    dlistint_t *saved_head;
+    dlistint_t *tmp;
+    unsigned int p;
 
     if (*head == NULL)
-        return (-1);
-
-    if (index == 0)
     {
-        *head = current->next;
-        if (current->next)
-            current->next->prev = NULL;
-        free(current);
-        return (1);
-    }
-
-    while (index > 0 && current != NULL)
-    {
-        current = current->next;
-        index--;
-    }
-
-    if (current == NULL)
         return (-1);
-
-    temp = current->prev;
-    temp->next = current->next;
-
-    if (current->next != NULL)
-        current->next->prev = temp;
-
-    free(current);
+    }
+    saved_head = *head;
+    p = 0;
+    while (p < index && *head != NULL)
+    {
+        *head = (*head)->next;
+        p++;
+    }
+    if (p != index || *head == NULL)
+    {
+        *head = saved_head;
+        return (-1);
+    }
+    if (0 == index)
+    {
+        tmp = (*head)->next;
+        free(*head);
+        *head = tmp;
+        if (tmp != NULL)
+        {
+            tmp->prev = NULL;
+        }
+    }
+    else
+    {
+        (*head)->prev->next = (*head)->next;
+        if ((*head)->next)
+            (*head)->next->prev = (*head)->prev;
+        free(*head);
+        *head = saved_head;
+    }
     return (1);
 }
